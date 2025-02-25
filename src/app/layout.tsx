be65684852cf1a98ae14c4b8af8864fdd8f123d6
ROHITH MP <rohithmp2014@gmail.com>
2025-02-25 23:59:09 +0530

    * unwantedline removed,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@src/components/theme-provider";
-import DarkModeToggle from "./DarkMode/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
